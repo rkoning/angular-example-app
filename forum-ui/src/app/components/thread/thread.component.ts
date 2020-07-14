1cdf8bf1388c3edd16fca964a8f7e9f9334f2af9
Ryan Koning <rkoning@textronsystems.com>
2020-07-14 16:38:45 -0600

    Added thread cards and form

--- forum-ui/src/app/components/thread/thread.component.ts
+++ forum-ui/src/app/components/thread/thread.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Thread } from '@models/thread.model';
+import { CommentRoutesService } from '@routes/comment-routes.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ThreadService } from '@services/thread.service';
 
 @Component({
   selector: 'app-thread',
@@ -6,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./thread.component.scss']
 })
 export class ThreadComponent implements OnInit {
+  @Input() thread: Thread;
 
-  constructor() { }
+  public comments: Comment[];
+  public showSpinner = true;
+  constructor(private route: ActivatedRoute, private threadService: ThreadService, private commentRoutesService: CommentRoutesService) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.thread = this.threadService.getThreadById(id);
+    console.log(this.thread);
+    // this.commentRoutesService.subscribe
+    setTimeout(() => {
+      this.showSpinner = false;
+    }, 3000);
   }
-
 }
--- forum-ui/src/app/models/thread.model.ts
+++ forum-ui/src/app/models/thread.model.ts
@@ -1,6 +1,13 @@
 export class Thread {
   id: string;
-  userId: string;
+  user: string;
   title: string;
   text: string;
+
+  constructor(id, user, title, text) {
+    this.id = id;
+    this.user = user;
+    this.title = title;
+    this.text = text;
+  }
 }
