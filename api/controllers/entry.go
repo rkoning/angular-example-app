d79f68a31c0130bb52eddddef0a79a82d8bd78e9
Joel Gilbert <gilbert.279@gmail.com>
2020-07-14 15:28:51 -0600

    added basic launch for api

--- api/controllers/entry.go
+++ api/controllers/entry.go
@@ -1,34 +1,85 @@
 package controllers
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"time"
 
+	"github.com/bitly/go-simplejson"
+
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-type Todo struct {
-	ID        string    `json:"id"`
-	Title     string    `json:"title"`
-	Body      string    `json:"body"`
-	Completed string    `json:"completed"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+// Thread is a main thread
+type Thread struct {
+	ID     string `bson:"_id" json:"id"`
+	Title  string `json:"title"`
+	Text   string `json:"text"`
+	UserID string `json:"userId"`
+}
+
+// Comment is a reply to the Thread
+type Comment struct {
+	ID       string `bson:"_id" json:"id"`
+	UserID   string `json:"userId"`
+	ParentID string `json:"parentId"`
+	Text     string `json:"text"`
 }
 
 // DATABASE INSTANCE
-var collection *mongo.Collection
+var threadCollection *mongo.Collection
+var commentCollection *mongo.Collection
 
-// EntriesCollection is a collection in mongo
-func EntriesCollection(c *mongo.Database) {
-	collection = c.Collection("entries")
+// ThreadsCollection is a collection in mongo
+func ThreadsCollection(c *mongo.Database) {
+	threadCollection = c.Collection("threads")
 }
 
-// GetAPIHandler Get Handler
-func GetAPIHandler(w http.ResponseWriter, r *http.Request) {
-	// entries :=
-	w.Write([]byte("Gorilla!\n"))
+// CommentsCollection is a collection of comments
+func CommentsCollection(c *mongo.Database) {
+	commentCollection = c.Collection("comments")
+}
+
+// GetAllThreads Get Handler
+func GetAllThreads(w http.ResponseWriter, r *http.Request) {
+	threads := []Thread{}
+
+	_, err := threadCollection.InsertOne(context.TODO(), bson.D{
+		{Key: "Title", Value: "Inserted From controller?"},
+		{Key: "Text", Value: "Wow, this was inserted from the controller!"},
+		{Key: "UserID", Value: "1234"}})
+
+	if err != nil {
+		log.Printf("Error Saving Data, Reason %v\n", err)
+	}
+
+	cursor, err := threadCollection.Find(context.TODO(), bson.M{})
+	if err != nil {
+		log.Printf("Error while getting all todos, Reason: %v\n", err)
+		return
+	}
+
+	// Iterate through the returned cursor.
+	for cursor.Next(context.TODO()) {
+		var thread Thread
+		cursor.Decode(&thread)
+		threads = append(threads, thread)
+	}
+
+	json := simplejson.New()
+	json.Set("data", threads)
+
+	payload, err := json.MarshalJSON()
+	if err != nil {
+		log.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(payload)
+	return
 }
 
 // PostAPIHandler for post functions
