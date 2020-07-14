f09bab48f54bae129588ebf6167afada33c45365
Joel Gilbert <gilbert.279@gmail.com>
2020-07-14 17:17:24 -0600

    added more endpoints

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -8,24 +8,11 @@ import (
 
 // Routes handles all the routes
 func Routes(r *mux.Router) {
+	r.HandleFunc("/threads/{id}/comments", controllers.GetAllCommentsForThread).Methods("GET").Schemes("http")
 	r.HandleFunc("/threads", controllers.GetAllThreads).Methods("GET").Schemes("http")
-	r.HandleFunc("/", controllers.PostAPIHandler).Methods("POST").Schemes("http")
-	r.HandleFunc("/", controllers.PutAPIHandler).Methods("Put").Schemes("http")
-	r.HandleFunc("/", controllers.DeleteAPIHandler).Methods("DELETE").Schemes("http")
+	r.HandleFunc("/threads/{id}/comments", controllers.AddCommentHandler).Methods("POST").Schemes("http")
+	r.HandleFunc("/threads", controllers.AddThreadHandler).Methods("POST").Schemes("http")
+	// r.HandleFunc("/", controllers.PutAPIHandler).Methods("Put").Schemes("http")
+	r.HandleFunc("/threads/{id}/comments/{commentId}", controllers.DeleteCommentHandler).Methods("DELETE").Schemes("http")
+	r.HandleFunc("/threads/{id}", controllers.DeleteThreadHandler).Methods("DELETE").Schemes("http")
 }
-
-// func welcome(c *gin.Context) {
-// 	c.JSON(http.StatusOK, gin.H{
-// 		"status":  200,
-// 		"message": "Welcome To API",
-// 	})
-// 	return
-// }
-
-// func notFound(c *gin.Context) {
-// 	c.JSON(http.StatusNotFound, gin.H{
-// 		"status":  404,
-// 		"message": "Route Not Found",
-// 	})
-// 	return
-// }
